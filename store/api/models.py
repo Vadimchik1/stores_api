a3a6e4ce274853d2185d806bd5eae322b4bde42c
Vadim Ganitsa <vadimganica@gmail.com>
2021-10-11 21:31:02 +0600

    Model was created

--- store/api/models.py
+++ store/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Store(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название')
+
+
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название')
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название')
+    store = models.ForeignKey(Store, verbose_name='Магазин', on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(ProductCategory, verbose_name='Категория товара', on_delete=models.SET_NULL, null=True)
